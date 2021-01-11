fbb578504770e14f8d9c781172be031e1f32af93
Vaibhavi <vaibhavi.golechha111.com>
2021-01-11 10:30:39 +0530

    test

--- nodejs-server/server.js
+++ nodejs-server/server.js
@@ -38,7 +38,7 @@ app.use("*", (req, res, next) => {
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
